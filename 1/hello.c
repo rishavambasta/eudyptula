f56bb557d9e3fce2002b157ed925122fa251cebb
Rishav Ambasta <kooolrishav@gmail.com>
2016-09-22 08:17:55 +0530

    Added initial Code, First Challenge, Hello World!

---
+++ 1/hello.c
@@ -0,0 +1,37 @@
+/*  hello.c - Prints "Hello World!" in the kernel logs
+ *  Author       : Rishav Ambasta <kooolrishav@gmail.com>
+ *  Date Created : 22/09/2016
+ */
+/* Kernel Programming */
+#define MODULE
+#define LINUX
+#define __KERNEL__
+
+#include <linux/module.h>
+#include <linux/kernel .h>
+#include <linux/init.h>
+
+
+/* Prototyping init funtions */
+int hello_init (void);
+void hello_cleanup (void);
+
+
+static int hello_init (void)
+{
+	printk (KERN_ALERT "Hello World!\n");
+	return 0;
+}
+
+static void hello_cleanup (void)
+{
+	printk (KERN_ALERT "Unloading Hello\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
+
+MODULE_LICENSE ("GPL"); //Not tainting the kernel
+
+MODULE_AUTHOR ("Rishav Ambasta <kooolrishav@gmail.com>");
+MODULE_DESCRIPTION ("A module to log Hello World!");
